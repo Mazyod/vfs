f5ffd19fbcf7fefe53c5d39594b8e8bdb7c2f2a8
Suraj Shirvankar <surajshirvankar@gmail.com>
2022-03-03 21:28:02 +0100

    Exit loop implemented

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <string>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::string command;
+    std::cout << "Supported commands: mkdir, touch, find, exit" << std::endl;
+
+    do{
+        std::cout << "Command: ";
+        std::cin >> command;
+
+        if(command != "exit"){
+            std::cout << "Invalid Command: " << command << std::endl;
+        }
+
+    }while(command != "exit");
+
+
+
     return 0;
 }
