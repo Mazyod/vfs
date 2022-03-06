b4c66b1879cb51183b40a612869849104c706d28
Suraj Shirvankar <surajshirvankar@gmail.com>
2022-03-06 18:13:39 +0100

    More comments and refactors

--- src/inmemory_file_system.cpp
+++ src/inmemory_file_system.cpp
@@ -4,82 +4,6 @@
 
 #include "inmemory_file_system.h"
 
-//Recursively create node based on the path vector
-void createDir(Tree &node, std::vector<std::string> &paths) {
-    while (!paths.empty()) {
-        auto currentPath = paths.begin();
-        Tree newNode(*currentPath, DIRECTORY);
-
-        Tree *child = nullptr;
-        node.get_child(*currentPath, child);
-
-        int pathSize = paths.size();
-        paths.erase(paths.begin(), paths.begin() + 1);
-//        Check if node with the name exists
-        if (child != nullptr) {
-//            Dont create a new directory if file with same name is present
-            if (child->nodeType == File) {
-                std::cout << "Invalid: Cannot create " << paths.back() << " under the File " << child->name
-                          << std::endl;
-                paths.erase(paths.begin(), paths.end());
-                return;
-            }
-//            Override directory if folder with the same name is present
-            if (pathSize == 0) {
-                *child = newNode;
-                return;
-            }
-            createDir(*child, paths);
-        } else {
-//            If no node is present create directory recursively
-            std::cout << newNode.name << " DIRECTORY created" << std::endl;
-            createDir(newNode, paths);
-            node.addTree(newNode);
-        }
-    }
-}
-
-void createFile(Tree &node, std::vector<std::string> &paths) {
-    while (!paths.empty()) {
-        auto currentPath = paths.begin();
-        Tree newNode(*currentPath, File);
-
-        Tree *child = nullptr;
-        node.get_child(*currentPath, child);
-
-        if (child != nullptr) {
-            if (child->nodeType == File and child->name == *currentPath and paths.size() == 1) {
-                *child = newNode;
-                std::cout << *currentPath << " has been created" << std::endl;
-                paths.erase(paths.begin(), paths.end());
-                return;
-            } else if (child->nodeType == File) {
-                std::cout << "Cannot create File " << paths.back() << " under the File " << *currentPath
-                          << std::endl;
-                paths.erase(paths.begin(), paths.end());
-                return;
-            } else {
-                if (paths.size() == 1) {
-                    std::cout << "Cannot create File " << paths.back() << " under the folder with same name "
-                              << *currentPath << std::endl;
-                    paths.erase(paths.begin(), paths.end());
-                    return;
-                }
-                paths.erase(paths.begin(), paths.begin() + 1);
-                createFile(*child, paths);
-            }
-        } else {
-            if (paths.size() == 1) {
-                std::cout << *currentPath << " has been created" << std::endl;
-                node.addTree(newNode);
-            } else {
-                std::cout << "Invalid: " << *currentPath << " does not exist" << std::endl;
-            }
-            paths.erase(paths.begin(), paths.end());
-            return;
-        }
-    }
-}
 
 //Splits the path string into a path vector and creates a new directory
 void InmemoryFileSystem::create_directory(std::string path) {
@@ -116,7 +40,115 @@ void InmemoryFileSystem::find_path(std::string path) {
     }
 }
 
-bool pathBetween(std::string first, std::string second, Tree root, std::vector<std::string> &path) {
+//Finds the path from the file1 to file2
+void InmemoryFileSystem::find_between_path(std::string first, std::string second) {
+    std::vector<std::string> pathV;
+    pathBetween(first, second, this->root, pathV);
+    if (pathV.empty()) {
+        std::cout << "No path can be found between " << first << " and " << second << std::endl;
+    } else {
+        for (auto it = pathV.begin(); it != pathV.end(); ++it) {
+            std::cout << *it;
+            if (it + 1 != pathV.end()) {
+                std::cout << " - ";
+            }
+        }
+        std::cout << std::endl;
+    }
+}
+
+//Recursively create node based on the path vector
+void InmemoryFileSystem::createDir(Tree &node, std::vector<std::string> &paths) {
+    while (!paths.empty()) {
+        auto currentPath = paths.begin();
+        Tree newNode(*currentPath, DIRECTORY);
+
+        Tree *child = nullptr;
+        node.get_child(*currentPath, child);
+
+        int pathSize = paths.size();
+        paths.erase(paths.begin(), paths.begin() + 1);
+
+//        Check if node with the name exists
+        if (child != nullptr) {
+
+//            Override directory if folder with the same name is present
+            if (child->nodeType ==DIRECTORY and pathSize == 0) {
+                *child = newNode;
+                return;
+            }
+
+//            Dont create a new directory if file with same name is present
+            if (child->nodeType == File) {
+                std::cout << "Invalid: Cannot create " << paths.back() << " under the File " << child->name
+                          << std::endl;
+//                stop iterating
+                paths.erase(paths.begin(), paths.end());
+                return;
+            }
+
+//            Create directories recursively
+            createDir(*child, paths);
+        } else {
+//            If no node is present create directory recursively
+            std::cout << newNode.name << " DIRECTORY created" << std::endl;
+            createDir(newNode, paths);
+            node.addTree(newNode);
+        }
+    }
+}
+
+//Recursively create node based on the path vector
+void InmemoryFileSystem::createFile(Tree &node, std::vector<std::string> &paths) {
+    while (!paths.empty()) {
+        auto currentPath = paths.begin();
+        Tree newNode(*currentPath, File);
+
+        Tree *child = nullptr;
+        node.get_child(*currentPath, child);
+
+        if (child != nullptr) {
+//            Override the existing file
+            if (child->nodeType == File and child->name == *currentPath and paths.size() == 1) {
+                *child = newNode;
+                std::cout << *currentPath << " has been created" << std::endl;
+                paths.erase(paths.begin(), paths.end());
+                return;
+            }
+//            Cannot create file under another file
+            if (child->nodeType == File) {
+                std::cout << "Cannot create File " << paths.back() << " under the File " << *currentPath
+                          << std::endl;
+                paths.erase(paths.begin(), paths.end());
+                return;
+            }
+//          Dont create file with the same name as directory
+            if (paths.size() == 1) {
+                std::cout << "Cannot create File " << paths.back() << " under the folder with same name "
+                          << *currentPath << std::endl;
+                paths.erase(paths.begin(), paths.end());
+                return;
+            }
+
+//          Recursively create file
+            paths.erase(paths.begin(), paths.begin() + 1);
+            createFile(*child, paths);
+
+        } else {
+            if (paths.size() == 1) {
+                std::cout << *currentPath << " has been created" << std::endl;
+                node.addTree(newNode);
+            } else {
+                std::cout << "Invalid: " << *currentPath << " does not exist" << std::endl;
+            }
+            paths.erase(paths.begin(), paths.end());
+            return;
+        }
+    }
+}
+
+//Recursively find path between two nodes with a shared root node
+bool InmemoryFileSystem::pathBetween(std::string first, std::string second, Tree root, std::vector<std::string> &path) {
     std::vector<std::string> pathFirst;
     bool foundFirst = root.find_path(first, pathFirst);
     if (!foundFirst) return false;
@@ -139,21 +171,4 @@ bool pathBetween(std::string first, std::string second, Tree root, std::vector<s
     }
 
     return false;
-}
-
-//Finds the path from the file1 to file2
-void InmemoryFileSystem::find_between_path(std::string first, std::string second) {
-    std::vector<std::string> pathV;
-    pathBetween(first, second, this->root, pathV);
-    if (pathV.empty()) {
-        std::cout << "No path can be found between " << first << " and " << second << std::endl;
-    } else {
-        for (auto it = pathV.begin(); it != pathV.end(); ++it) {
-            std::cout << *it;
-            if (it + 1 != pathV.end()) {
-                std::cout << " - ";
-            }
-        }
-        std::cout << std::endl;
-    }
 }
